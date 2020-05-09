f46760994a4fdb6faf4824c46bc2f01345a94a4b
Tom Kazimiers <tom@voodoo-arts.net>
2020-05-08 22:19:26 -0400

    Provide back-end config to front-end
    At least partially this is needed to link import to a source
    identifier.

--- circuitmap/apps.py
+++ circuitmap/apps.py
@@ -2,6 +2,16 @@ from __future__ import unicode_literals
 
 from django.apps import AppConfig
 
-
 class CircuitmapConfig(AppConfig):
     name = 'circuitmap'
+
+    def get_config(self):
+        from circuitmap import control
+        return {
+            'seg_name': control.SEGMENTATION_NAME,
+            'seg_access_url': control.CLOUDVOLUME_URL,
+            'seg_storage': control.GOOGLE_SEGMENTATION_STORAGE,
+            'skeleton_key': control.CLOUDVOLUME_SKELETONS,
+            'import_user': control.DEFAULT_IMPORT_USER,
+            'connector_id_offset': control.CONNECTORID_OFFSET,
+        }
